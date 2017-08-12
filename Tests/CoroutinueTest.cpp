98888b13c053dd7f3fb61e5afe51124fa568ff3d
adrian <whitespace@163.com>
2017-08-12 15:35:12 +0800

    添加协程库

---
+++ Tests/CoroutinueTest.cpp
@@ -0,0 +1,48 @@
+//
+// Created by Adrian on 2017/8/12.
+//
+
+#include "../Headers.h"
+#include "Parttern/Coroutine.h"
+
+using namespace std;
+
+void test1(void* arg)
+{
+    CoroutinueContext& context = *(CoroutinueContext*)arg;
+
+    cout << "Coroutine 1 before yield" << endl;
+
+    context.YieldReturn();
+
+    cout << "Coroutine 1 after yield" << endl;
+
+}
+
+void test2(void* arg)
+{
+    CoroutinueContext& context = *(CoroutinueContext*)arg;
+
+    cout << "Coroutine 2 before yield" << endl;
+
+    context.YieldReturn();
+
+    cout << "Coroutine 2 after yield" << endl;
+
+}
+
+int main() {
+
+    CoroutinueContext context;
+
+    int c1 = context.Create(test1, &context);
+    int c2 = context.Create(test2, &context);
+
+    while (!context.Finished())
+    {
+        context.Resume(c1);
+        context.Resume(c2);
+    }
+
+    return 0;
+}
